09ff4bbdc0e52f82dd6e53ff4d248d06d455b4e6
Dmitry <98712561+dg-korolev@users.noreply.github.com>
2025-01-08 23:04:16 +0300

    feat(nx-remote-cache-redis): init (#5)
    * feat(nx-remote-cache-redis): init
    
    ---------
    
    Co-authored-by: Dmitry Korolev <korolev.dgk@gmail.com>

---
+++ packages/nx-remote-cache-redis/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nx-remote-cache-redis',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/nx-remote-cache-redis',
+};
---
+++ packages/nx-remote-cache-redis/src/index.spec.ts
@@ -0,0 +1,5 @@
+describe('empty', () => {
+  it('empty', async () => {
+    return;
+  });
+});
